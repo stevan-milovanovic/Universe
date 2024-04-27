0d8a90674b315fecdb3651564c3eae5395fd4e31
Stevan Milovanovic <stevanm993@gmail.com>
2024-04-27 04:07:36 +0200

    Add application class

---
+++ app/src/main/java/rs/smobile/universe/UniverseApp.kt
@@ -0,0 +1,10 @@
+package rs.smobile.universe
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * [Application] class for Universe App
+ */
+@HiltAndroidApp
+class UniverseApp : Application()
